ed4fc4eb48cc966ad15cdf98f17ea61b310f22ef
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:49:04 +0500

    Task-6 Added

---
+++ src/Q6.java
@@ -0,0 +1,7 @@
+//Matrix Multiplication:
+//Write a multithreaded program to multiply two matrices. Divide the task among multiple threads
+//to perform partial matrix multiplications concurrently.
+
+public class Q6 {
+
+}
