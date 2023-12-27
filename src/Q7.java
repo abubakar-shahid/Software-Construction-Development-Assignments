1e2a022cb1ff36f21ee0dcd1f783598bd1554b97
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:50:48 +0500

    Task-7 Added

---
+++ src/Q7.java
@@ -0,0 +1,8 @@
+//Concurrent Search:
+//Implement a program that searches for a specific element in a large array concurrently
+//using multiple threads. Each thread should search a distinct portion of the array, and
+//the program should output the index if the element is found.
+
+public class Q7 {
+
+}
