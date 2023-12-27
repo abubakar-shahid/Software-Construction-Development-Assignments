72a3fb56c00f866282abc9f3c0d5dc7d3c31f86e
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:47:33 +0500

    Task-4 Added

---
+++ src/Q4.java
@@ -0,0 +1,8 @@
+//Bank Transactions:
+//Simulate a simple banking system where multiple threads represent different account holders
+//performing deposits and withdrawals concurrently. Ensure that the transactions are thread-safe
+//to avoid data corruption.
+
+public class Q4 {
+
+}
