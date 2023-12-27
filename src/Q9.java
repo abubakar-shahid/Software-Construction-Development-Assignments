dd7acb25279895f706fce76f71d610acc9e642ec
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:51:46 +0500

    Task-9 Added

---
+++ src/Q9.java
@@ -0,0 +1,7 @@
+//Parallel Sorting:
+//Design a program that sorts an array of integers using a parallel sorting algorithm.
+//Divide the array into segments and use multiple threads to sort each segment concurrently.
+
+public class Q9 {
+
+}
