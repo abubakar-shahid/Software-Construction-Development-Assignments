3fcd44fa2c52747457db18bee5dbef02d284b3af
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:46:22 +0500

    Task-3 Added

---
+++ src/Q3.java
@@ -0,0 +1,8 @@
+//Reader-Writer Problem:
+//Write a Java program to solve the reader-writer problem using threads.
+//Readers can read simultaneously, but writers must have exclusive access to the shared resource.
+//Ensure that readers and writers do not conflict with each other.
+
+public class Q3 {
+
+}
