c8c7e6952ad28f77e3bc6f3d9d2ac5d3076f7ead
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:52:30 +0500

    Task-10 Added

---
+++ src/Q10.java
@@ -0,0 +1,7 @@
+//Thread Pool Implementation:
+//Implement a basic thread pool with a fixed number of worker threads. Clients can submit tasks
+//to the thread pool for execution, and the threads should execute these tasks concurrently.
+
+public class Q10 {
+
+}
