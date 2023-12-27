4e07fb506cfcc7ee2747a0ad6c9f701af200e8fa
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 21:00:23 +0500

    Configuration Interfce Added!

---
+++ src/Configuration.java
@@ -0,0 +1,13 @@
+//package Assignment1;
+
+public interface Configuration {
+
+    public void displayInfo();
+
+    public double calculateCost();
+
+    public boolean checkBorrower(Borrower b);
+
+    public String getData();
+
+}
