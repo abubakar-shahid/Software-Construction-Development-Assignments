582f4e7b037fe6fa7c9e4b4932f6ef9378b0f935
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:48:19 +0500

    Task-5 Added

---
+++ src/Q5.java
@@ -0,0 +1,7 @@
+//Print Even and Odd Numbers:
+//Create two threads, one for printing even numbers and another for printing odd numbers.
+//Implement a mechanism to ensure that the numbers are printed in sequential order.
+
+public class Q5 {
+
+}
