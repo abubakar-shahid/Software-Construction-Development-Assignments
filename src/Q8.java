b61239b74daa30e48bd61b3105738dff4074c0df
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-27 23:50:57 +0500

    Task-8 Added

---
+++ src/Q8.java
@@ -0,0 +1,8 @@
+//Thread Communication:
+//Create two threads: one for producing an item and another for consuming it.
+//Use wait() and notify() or notifyAll() to coordinate the communication between
+//the producer and consumer threads.
+
+public class Q8 {
+
+}
